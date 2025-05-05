36c8b9be11e1c37f361a8498e4c889c0cdd2686d
Divine IGIHOZO <divine.igihozo@gmail.com>
2025-05-05 03:13:02 -0400

    Test this for me!

--- ShuSpotFinder/FindParkingViewController.swift
+++ ShuSpotFinder/FindParkingViewController.swift
@@ -4,26 +4,41 @@
 //
 //  Created by d.igihozo on 5/4/25.
 //
-
 import UIKit
+import MapKit
+import CoreLocation
 
-class FindParkingViewController: UIViewController {
+class FindParkingViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
 
-        // Do any additional setup after loading the view.
+        // Set initial region to university
+        let campusLocation = CLLocationCoordinate2D(latitude: 40.309, longitude: -79.556)
+        let region = MKCoordinateRegion(center: campusLocation, latitudinalMeters: 500, longitudinalMeters: 500)
+        mapView.setRegion(region, animated: true)
+        mapView.showsUserLocation = true
+        //Zoom in and out
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        mapView.isPitchEnabled = true
+        mapView.isRotateEnabled = true
+
+
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
+       
+
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+extension FindParkingViewController: MKMapViewDelegate {
 }
--- ShuSpotFinder/NewAccountViewController.swift
+++ ShuSpotFinder/NewAccountViewController.swift
@@ -6,24 +6,49 @@
 //
 
 import UIKit
+import CoreData
 
 class NewAccountViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBAction func signUpButtonTapped(_ sender: UIButton) {
+        
+        guard let username = usernameTextField.text,
+              let password = passwordTextField.text,
+              let confirmPassword = confirmPasswordTextField.text,
+              password == confirmPassword else {
+            print("passwords don't match")
+            return
+        }
+        
+        // Save to Core Data
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let newUser = User(context: context)
+        newUser.username = username
+        newUser.password = password
+        
+        
+        do {
+            try context.save()
+            print("User saved!")
+            // After saving, transition to the FindParking ViewController
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            if let findParkingVC = storyboard.instantiateViewController(withIdentifier: "FindParkingViewController") as? FindParkingViewController {
+                self.navigationController?.pushViewController(findParkingVC, animated: true)
+            }
+            
+        } catch {
+            print("Failed to save user: \(error)")
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
     }
-    */
-
 }
